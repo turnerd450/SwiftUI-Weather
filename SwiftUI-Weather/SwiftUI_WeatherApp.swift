95eb1aef7dff82146ff2aa4cc7ce1ff4c9f570db
Delwin Turner <turnerd@yahoo.com>
2022-04-08 20:05:15 -0600

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by DELWIN TURNER on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
